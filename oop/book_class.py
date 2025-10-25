2829f1d80d30e49015d35de8c170d80a5920acd7
Cyrus Maina <77388640+Cyr0707@users.noreply.github.com>
2025-10-25 16:11:09 +0300

    Update book_class.py

--- oop/book_class.py
+++ oop/book_class.py
@@ -1,9 +1,12 @@
 class Book:
+    """
+    Represents a book with a title, author, and publication year.
+    """
+
     def __init__(self, title, author, year):
         self.title = title
         self.author = author
         self.year = year
-        print(f"Book '{self.title}' created.")
 
     def __str__(self):
         return f"{self.title} by {self.author}, published in {self.year}"
