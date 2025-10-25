3de1c6cee64371abbb404a1025219487a1e5f0f2
Cyrus Maina <77388640+Cyr0707@users.noreply.github.com>
2025-10-25 16:01:07 +0300

    book_class.py doesn't contain: ["__str__(self):", "__repr__(self):"]

--- oop/book_class.py
+++ oop/book_class.py
@@ -19,25 +19,25 @@ class Book:
 
     def __str__(self) -> str:
         """
-        String Representation: Returns a user-friendly string format.
+        String Representation (__str__): Returns a user-friendly string format.
+        Format: "(title) by (author), published in (year)"
         This is used by print() and str().
         """
         return f"{self.title} by {self.author}, published in {self.year}"
 
     def __repr__(self) -> str:
         """
-        Official Representation: Returns an unambiguous string that can 
-        be used to recreate the object.
+        Official Representation (__repr__): Returns an unambiguous string that 
+        can be used to recreate the object.
+        Format: "Book('(title)', '(author)', (year))"
         This is used by repr().
         """
+        # Note the use of quotes around title and author to make it a valid constructor call
         return f"Book('{self.title}', '{self.author}', {self.year})"
 
     def __del__(self):
         """
-        Destructor: Called when the object is about to be destroyed (deleted).
+        Destructor (__del__): Called when the object is about to be destroyed.
         Prints a cleanup message.
         """
-        # The destructor is called when the reference count of the object drops to zero.
         print(f"Deleting {self.title}")
-
-# Note: The provided main.py will handle the creation and testing.
